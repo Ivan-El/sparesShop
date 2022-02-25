16fc8f34eb4ceb6af608fd31c769e6842add0768
Иван <lostnemo@rambler.ru>
2022-02-25 08:29:02 +0300

    Добавляет в скрипт открытия меню проверку на работоспособность js

--- source/js/menu.js
+++ source/js/menu.js
@@ -2,9 +2,11 @@
 
 let menuButton = document.querySelector(".hamburger");
 
+document.body.classList.remove("no-js")
+
 if (menuButton) {
   menuButton.addEventListener('click', function() {
     document.body.classList.toggle("menu-opened");
-    menuButton.classList.toggle("hamburger--opened")
+    menuButton.classList.toggle("hamburger--opened");
   });
 }
