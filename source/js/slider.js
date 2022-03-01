d2e31df39c07629e25afb19e9d746a58c49de24c
Иван <lostnemo@rambler.ru>
2022-03-01 06:34:13 +0300

    Добавляет разметку, элементы и стили для слайдера акционных товаров

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,30 +1,26 @@
 "use strict"
 
-let slideIndex = 1;
-let arrows = document.querySelectorAll(".slider__arrow");
-let dots = document.querySelectorAll(".slider__dot");
+let slideIndex = 0;
+let sliderArrows = document.querySelectorAll(".slider__arrow");
+let dotsArrows = document.querySelectorAll(".slider__dot");
 
 showSlides(slideIndex);
 
-if (arrows) {
-  for (let arrow of arrows) {
-    if (arrow.classList.contains(".slider__arrow--next")){
-      arrow.addEventListener("click", function(){
-          showSlides(slideIndex += 1);
-        })
-    } else {
-        arrow.addEventListener("click", function(){
-          showSlides(slideIndex -= 1);
-        })
-    }
+if (sliderArrows) {
+  for (let arrow of sliderArrows) {
+    arrow.classList.contains("slider__arrow--previous") ?
+    arrow.addEventListener("click", () => {
+      showSlides(slideIndex -= 1)}) :
+    arrow.addEventListener("click", () => {
+      showSlides(slideIndex += 1)});
   }
 }
 
-if(dots) {
-  for (let i = 0; i < dots.length; i++) {
-    let dot = dots[i];
+if(dotsArrows) {
+  for (let i = 0; i < dotsArrows.length; i++) {
+    let dot = dotsArrows[i];
     dot.addEventListener("click", function(){
-      showSlides(slideIndex = i + 1);
+      showSlides(slideIndex = i);
     })
   }
 }
@@ -32,17 +28,17 @@ if(dots) {
 function showSlides(n) {
   let slides = document.querySelectorAll(".slider__slide");
 
-  if (n > slides.length) {
-    slideIndex = 1;
-  } else if (n < 1) {
-    slideIndex = slides.length;
+  if (n > slides.length-1) {
+    slideIndex = 0;
+  } else if (n < 0) {
+    slideIndex = slides.length-1;
   }
 
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
-    dots[i].classList.remove("slider__dot--current");
+    dotsArrows[i].classList.remove("slider__dot--current");
   }
 
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].classList.add("slider__dot--current");
+  slides[slideIndex].style.display = "block";
+  dotsArrows[slideIndex].classList.add("slider__dot--current");
 }
