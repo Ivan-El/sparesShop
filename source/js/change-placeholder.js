af092b00eedcc0c28c7e5a336f4508f85f57d4a1
Иван <lostnemo@rambler.ru>
2022-03-01 18:33:42 +0300

    Добавляет скрипт меняющий текст в поиске в зависимости от радио кнопки

---
+++ source/js/change-placeholder.js
@@ -0,0 +1,20 @@
+"use strict"
+function changePlaceholder() {
+  let radioButtons= document.querySelectorAll(".search-form__radio-input");
+  let textInput = document.querySelector(".search-form__text-input");
+
+  let placeholders = {
+    vin: "Введите VIN автомобиля",
+    car_brand: "Введите марку автомобиля",
+    product_name: "Введите название товара",
+    article: "Введите артикул товара",
+  }
+
+  for (let radioButton of radioButtons) {
+    radioButton.addEventListener("click", () => {
+      textInput.placeholder = placeholders[radioButton.value];
+    })
+  }
+}
+
+changePlaceholder()
