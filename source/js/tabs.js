e4a46f61d28ba42e1d2178b64c07343b67e9d56e
Иван <lostnemo@rambler.ru>
2022-03-23 18:53:12 +0300

    Изменяет скрипт,добавляет возможность использовать несколько вкладок

--- source/js/tabs.js
+++ source/js/tabs.js
@@ -1,43 +1,28 @@
 "use strict"
 
-let tabsBody = document.querySelector(".tabs");
+let tabsList = document.querySelectorAll(".tabs__list");
 let tabLinks = document.querySelectorAll(".tabs__link");
 let tabPanels = document.querySelectorAll(".tabs__panel");
-let selectedTab = tabLinks[0];
-let selectedPanel = tabPanels[0];
 
+for (let tabLink of tabLinks) {
 
-tabsBody.addEventListener("click", function (evt) {
-  let target = evt.target;
-  let targetHref = target.getAttribute("href");
-  evt.preventDefault();
+  tabLink.addEventListener("click", function (evt) {
+    let target = evt.target;
+    let targetHref = target.getAttribute("href");
 
-  if (!target.classList.contains("tabs__link")) return;
+    evt.preventDefault();
 
-  activateTab(target);
-  activatePanel(targetHref)
-});
+    if (!target.classList.contains("tabs__link")) return;
 
+    target.closest(".tabs").querySelector(".tabs__link--current").classList.remove("tabs__link--current");
+    target.classList.add("tabs__link--current");
 
-function activateTab (tab) {
-  if (selectedTab) {
-    selectedTab.classList.remove("tabs__link--current");
-  }
+    target.closest(".tabs").querySelector(".tabs__panel--current").classList.remove("tabs__panel--current");
 
-  selectedTab = tab;
-  selectedTab.classList.add("tabs__link--current");
-};
-
-
-function activatePanel (tabHref) {
-  if (selectedPanel) {
-    selectedPanel.classList.remove("tabs__panel--current");
-  }
-
-  tabPanels.forEach((panel, i) => {
-    if (tabPanels[i].id === tabHref.slice(1)) {
-      selectedPanel = tabPanels[i];
-      selectedPanel.classList.add("tabs__panel--current");
-    }
+    tabPanels.forEach((panel, i) => {
+      if (tabPanels[i].id === targetHref.slice(1)) {
+        tabPanels[i].classList.add("tabs__panel--current");
+      }
+    });
   });
 }
